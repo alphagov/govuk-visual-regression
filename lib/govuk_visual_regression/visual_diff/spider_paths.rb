e9ff6acb2b7d2f3f3242428687d42524f8f7603a
Paul Hayes <fofr@users.noreply.github.com>
2017-09-18 14:23:02 +0100

    Only test pages with all a component’s examples
    * Keep number of pages to test down to a minimum
    * Only test the pages that show all previews on one page
    
    eg:
    "/component-guide/contents-list/preview"
    but not:
    "/component-guide/contents-list/long_text/preview"

--- lib/govuk_visual_regression/visual_diff/spider_paths.rb
+++ lib/govuk_visual_regression/visual_diff/spider_paths.rb
@@ -7,9 +7,11 @@ module GovukVisualRegression
         YAML.load_file(GovukVisualRegression.spider_paths)['paths']
       end
 
+      # Keep number of pages test down to a minimum
       # Only test component previews, not the guide itself
+      # And only test the pages that show all previews on one page
       def self.component_preview_paths
-        paths.values.select { |v| v.match(/preview$/) }
+        paths.values.select { |v| v.match(/preview$/) && v.split('/').length == 4 }
       end
     end
   end
